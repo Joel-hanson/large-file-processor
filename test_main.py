0ef81a58550d5ef10e5967be7111318a78dff365
Joel Hanson <joelhanson025@gmail.com>
2021-04-15 23:12:15 +0530

    Update redundent code

--- test_main.py
+++ test_main.py
@@ -7,11 +7,11 @@ from main import main, get_product_count, get_duplicate_count
 
 
 
+db_string = "postgresql://postgres:password@localhost:5432"
 def test_product_count():
     """
     Test the overall product count
     """
-    db_string = "postgresql://postgres:password@localhost:5432"
     db = create_engine(db_string)
     main(db)
     assert get_product_count(db) == (500000,)
@@ -22,7 +22,6 @@ def test_product_count_second_run():
     """
     Test the overall product run again
     """
-    db_string = "postgresql://postgres:password@localhost:5432"
     db = create_engine(db_string)
     main(db)
     assert get_product_count(db) == (500000,)
